6597d50199b78bb028e399fd627512a1bff1f779
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-27 21:27:10 -0600

    feat(projects/01-guitars-world): Finish guitars world project

--- projects/01-guitars-world/src/App.vue
+++ projects/01-guitars-world/src/App.vue
@@ -1,33 +1,116 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted, watch } from "vue"
+  import { db } from "./data/guitars"
+  import Guitar from "./components/Guitar.vue"
+  import Header from "./components/Header.vue"
+  import Footer from "./components/Footer.vue"
+
+  const guitarras = ref([])
+  const carrito = ref([])
+  const guitarraHeader = ref({})
+
+  watch(
+    carrito,
+    () => {
+      guardarLocalStorage()
+    },
+    {
+      deep: true,
+    }
+  )
+
+  onMounted(() => {
+    guitarras.value = db
+    guitarraHeader.value = db[3]
+
+    const carritoStorage = localStorage.getItem("carrito")
+    
+    if (carritoStorage) {
+      carrito.value = JSON.parse(carritoStorage)
+    }
+  })
+
+  const guardarLocalStorage = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito.value))
+  }
+
+  const agregarCarrito = (guitarra) => {
+    const existeCarrito = carrito.value.findIndex((producto) => producto.id === guitarra.id)
+    
+    if (existeCarrito >= 0) {
+      carrito.value[existeCarrito].cantidad++
+    } else {
+      alert("Se ha añadido la guitarra al carrito")
+      guitarra.cantidad = 1
+      carrito.value.push(guitarra)
+    }
+  }
+
+  const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex((producto) => producto.id === id)
+    if (carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+  }
+
+  const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex((producto) => producto.id === id)
+    if (carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+  }
+
+  const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter((producto) => producto.id !== id)
+  }
+
+  const vaciarCarrito = () => {
+    carrito.value = []
+  }
 </script>
 
 <template>
-  <div>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
+  <Header
+    :carrito="carrito"
+    :guitarraHeader="guitarraHeader"
+    @decrementar-cantidad="decrementarCantidad"
+    @incrementar-cantidad="incrementarCantidad"
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
+  />
 
-    <HelloWorld msg="Vite + Vue" />
-  </div>
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+    <div class="row mt-5">
+      <Guitar
+        v-for="guitarra in guitarras"
+        :key="guitarra.id"
+        :guitarra="guitarra"
+        @agregar-carrito="agregarCarrito"
+      />
+    </div>
+  </main>
+
+  <Footer />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="css">
+  /* width */
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #fff;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: orange;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 </style>
