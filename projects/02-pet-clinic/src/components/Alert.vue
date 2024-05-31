94ef8a71fb7831e8b376a86b6fc8ac6fd52e0f5f
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-30 19:28:15 -0600

    feat(projects/02-pet-clinic): Add structure components

---
+++ projects/02-pet-clinic/src/components/Alert.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { computed } from "vue"
+
+  const props = defineProps({
+    alert: {
+      type: Object,
+      required: true,
+    },
+  })
+
+  const isError = computed(() => {
+    return props.alert.type === "error"
+  })
+</script>
+
+<template>
+  <div
+    :class="[isError ? 'bg-red-500' : 'bg-green-500']"
+    class="text-center p-3 uppercase font-bold mb-3 mx-5 rounded-md"
+  >
+    {{ alert.message }}
+  </div>
+</template>
---
+++ projects/02-pet-clinic/src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="font-black text-5xl text-center">
+    Seguimiento pacientes <span class="text-violet-600">Veterinaria</span>
+  </h1>
+</template>
