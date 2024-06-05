eeaa119e04927e966d49f5de56ac53699efd7fa0
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-04 21:46:16 -0600

    feat(Root): Add components

---
+++ projects/04-crypto-coins/src/components/Quote.vue
@@ -0,0 +1,43 @@
+<script setup>
+  defineProps({
+    quote: {
+      type: Object,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <div class="container-result">
+    <h2>Cotización:</h2>
+
+    <div class="result">
+      <img
+        :src="'https://cryptocompare.com/' + quote.IMAGEURL"
+        alt="imagen crypto"
+      />
+      
+      <div>
+        <p>
+          El precio es de: <span>{{ quote.PRICE }}</span>
+        </p>
+
+        <p>
+          Precio más alto del día: <span>{{ quote.HIGHDAY }}</span>
+        </p>
+
+        <p>
+          Precio más bajo del día: <span>{{ quote.LOWDAY }}</span>
+        </p>
+
+        <p>
+          Variación últimas 24 hrs: <span>{{ quote.CHANGEPCT24HOUR }}%</span>
+        </p>
+
+        <p>
+          Última actualización: <span>{{ quote.LASTUPDATE }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
