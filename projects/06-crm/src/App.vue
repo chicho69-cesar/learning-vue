0b4d384ead431c4fe85b141df96f38ee67a59a25
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-06 18:27:21 -0600

    feat(Root): Configure project dependencies and assets

--- projects/06-crm/src/App.vue
+++ projects/06-crm/src/App.vue
@@ -1,36 +1,36 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
+  import { onMounted } from "vue"
+  import { RouterView } from "vue-router"
+
+  import Header from "./components/ui/Header.vue"
 </script>
 
 <template>
-  <div>
-    <div>
-      <a href='https://vitejs.dev' target='_blank'>
-        <img src='/vite.svg' class='logo' alt='Vite logo' />
-      </a>
+  <Header />
 
-      <a href='https://vuejs.org/' target='_blank'>
-        <img src='./assets/vue.svg' class='logo vue' alt='Vue logo' />
-      </a>
-    </div>
-
-    <HelloWorld msg='Vite + Vue' />
-  </div>
+  <main class="max-w-6xl mx-auto mt-10">
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+<style lang="css">
+  /* width */
+  ::-webkit-scrollbar {
+    width: 10px;
   }
 
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #fff;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #1e293b;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #0b1016;
   }
 </style>
