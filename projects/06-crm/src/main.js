97d642e22991b7a7102db4d2c59a05e9439cab7e
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-06 18:47:40 -0600

    feat(Root): Finish the project

--- projects/06-crm/src/main.js
+++ projects/06-crm/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue'
+
+import config from '../formkit.config'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(plugin, defaultConfig(config))
+
+app.mount('#app')
