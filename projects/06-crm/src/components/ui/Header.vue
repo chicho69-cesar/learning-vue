ac2ee19e89483a0fe464042b202a8c0cdac7cce2
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-06 18:46:30 -0600

    feat(Root): Add ui components

---
+++ projects/06-crm/src/components/ui/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="bg-slate-800">
+    <div class="mx-auto max-w-6xl py-10">
+      <h1 class="text-4xl font-extrabold text-white">Administrador de clientes</h1>
+      
+      <h3 class="font-extrabold text-white underline">
+        <a href="https://github.com/chicho69-cesar" target="_blank">Github</a>
+      </h3>
+    </div>
+  </header>
+</template>
