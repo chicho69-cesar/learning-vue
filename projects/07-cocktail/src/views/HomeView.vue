dd7804e35d245ab565952248ed54dfa822479935
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-07 21:39:31 -0600

    feat(Root): Add routing configuration

---
+++ projects/07-cocktail/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Recipe from '../components/Recipe.vue';
+  import { useDrinksStore } from '../stores/drinks';
+
+  const drinks = useDrinksStore();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-6xl font-extrabold">Recetas</h1>
+    <p v-if="drinks.noRecipes" class="my-10 text-center text-2xl">Realice una búsqueda</p>
+
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-2">
+      <Recipe v-for="recipe in drinks.recipes" :key="recipe.idDrink" :recipe="recipe" />
+    </div>
+  </div>
+</template>
