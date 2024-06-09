2d325685059609d6fbae83187c4cb43a3b3bd768
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-08 22:34:21 -0600

    feat(Root): Add validations schemas

---
+++ projects/08-property-sales/src/validation/loginSchema.js
@@ -0,0 +1,18 @@
+export const loginSchema = {
+  email(value) {
+    if (!value) {
+      return 'Este campo es obligatorio'
+    }
+
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    if (!regex.test(value)) {
+      return 'Email no válido'
+    }
+
+    return true
+  },
+  password(value) {
+    if (value) return true
+    return 'El Password es Obligatorio'
+  }
+}
