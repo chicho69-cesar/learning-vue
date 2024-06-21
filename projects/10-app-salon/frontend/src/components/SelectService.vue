8e4e2981c88f88337eaf098ee34d93110ec3b7b6
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-20 22:03:15 -0600

    feat(Root): Add app components

---
+++ projects/10-app-salon/frontend/src/components/SelectService.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import {useAppointmentStore} from '../stores/appointments'
+  import { formatCurrency } from "../helpers"
+
+  const appointments = useAppointmentStore()
+
+  const props = defineProps({
+    service: {
+      type: Object,
+    },
+  })
+</script>
+
+<template>
+  <div
+    class="border border-gray-600 rounded-lg p-5 flex justify-between items-center"
+  >
+    <div>
+      <p class="text-2xl font-extrabold text-white">
+        {{ service.name }}
+      </p>
+      
+      <p class="text-3xl font-black text-blue-500">
+        {{ formatCurrency(service.price) }}
+      </p>
+    </div>
+
+    <button @click="appointments.onServiceSelected(service)" type="button" class="inline-flex items-center rounded-md bg-red-600 text-white text-sm px-3 py-2 font-semibold shadow-sm ring-1 ring-inset ring-red-300 hover:bg-red-700">
+      Eliminar
+    </button>
+  </div>
+</template>
