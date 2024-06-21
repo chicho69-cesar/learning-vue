073581983ad8f5f2a6e8662d452446a4f681a6fb
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-06-20 22:03:48 -0600

    feat(Root): Add routing layouts

---
+++ projects/10-app-salon/frontend/src/views/auth/AuthLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { RouterView, RouterLink, useRoute } from "vue-router"
+
+  const route = useRoute()
+
+  const authRoutes = [
+    { name: "register", text: "Crear Cuenta" },
+    { name: "login", text: "Iniciar Sesión" },
+    { name: "forgot-password", text: "Olvidé mi password" },
+  ]
+</script>
+
+<template>
+  <div class="mx-auto lg:w-3/5 mt-20">
+    <RouterView />
+
+    <nav
+      class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0"
+    >
+      <RouterLink
+        v-for="authRoute in authRoutes"
+        :key="authRoute.name"
+        class="uppercase font-bold text-white"
+        :class="{ hidden: route.name === authRoute.name }"
+        :to="{ name: authRoute.name }"
+      >
+        {{ authRoute.text }}
+      </RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>
